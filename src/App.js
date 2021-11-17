58a85ae4f656bdcba044dbc0b578f194a83f3b46
Sebastian Syska <syska.seb@gmail.com>
2021-11-17 23:02:13 +0100

    test firestore usage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import {getFirestore, collection, addDoc, getDocs} from "firebase/firestore";
+import {useEffect, useState} from "react";
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCRPSk2Y5p-Yl6FHrf_3CBc-G30wb3x698",
+    authDomain: "hackathon-fd9ef.firebaseapp.com",
+    projectId: "hackathon-fd9ef",
+    storageBucket: "hackathon-fd9ef.appspot.com",
+    messagingSenderId: "792012328661",
+    appId: "1:792012328661:web:e0f5f17401bbf404ecdc5c"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [variables, setField] = useState([{field1: "", field2: ""}]);
+    // const [field2, setField2] = useState([{field2: ""}]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const db = getFirestore();
+            const col = await getDocs(collection(db, "test-collection"));
+            const data = [];
+            col.forEach((e) => {
+                data.push(e.data())
+            });
+            console.log(data);
+            setField(data[0]);
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <div>
+            <p>
+                {variables.field1}
+            </p>
+            <p>
+                {variables.field2}
+            </p>
+        </div>
+    );
 }
 
 export default App;
