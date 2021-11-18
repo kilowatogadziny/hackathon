fcbf5b2c1b74d576040ed1e4b7b2b50ce23dac2f
Dominika <dominikasaide@gmail.com>
2021-11-18 23:03:17 +0100

    Added Songview - bare minimum firebase config in a separate file

--- src/SongView.js
+++ src/SongView.js
@@ -1,28 +1,62 @@
 import React from "react";
 import "regenerator-runtime/runtime.js";
-import {collection, addDoc, doc, getDoc} from "firebase/firestore";
+import {doc, getDoc} from "firebase/firestore";
+import {useEffect, useState} from "react";
 import {db} from "./firebaseConfig"
 
-export default function SongView({songOfDay}) {
+export default function SongView({id}) {
 
-    const saveSong = async (song) => {
-        const docRef = await addDoc(collection(db, "songs"), {
-            artist: song.artist,
-            album: song.album,
-            song: song.title,
-            date: new Date().getDate(),
-        });
-        console.log("Document written with ID: ", docRef.id);
+    const [song, setSong] = useState([{
+        album_title: "",
+        artist_name: "",
+        cover_url: "",
+        date: "",
+        song_title: ""
+    }]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const documentId = id;
+            let songOfDay = {};
+            await getSnapshot(documentId).then((snapshot) => {
+                if (snapshot.exists()) {
+                    songOfDay = toSongOfDayObject(snapshot);
+                } else {
+                    console.log("No such document!" + documentId);
+                }
+
+            })
+            console.log(songOfDay);
+            setSong(songOfDay);
+        }
+        fetchData();
+    }, []);
+
+    const getSnapshot = async (id) => {
+        const songsRef = doc(db, "songs", id);
+        return await getDoc(songsRef);
+    }
+
+    const toSongOfDayObject = (snapshot) => {
+        const data = snapshot.data();
+        const song = {
+            album_title: data.album_title,
+            artist_name: data.artist_name,
+            cover_url: data.cover_url,
+            date: data.date,
+            song_title: data.song_title
+        }
+        return song;
     }
 
     return (
         <div>
-            <p>{songOfDay.date}</p>
-            <p>{songOfDay.album_title}</p>
-            <img src={songOfDay.cover_url}/>
-            <p>{songOfDay.artist_name}</p>
-            <p>{songOfDay.album_title}</p>
-            <p>{songOfDay.song_title}</p>
+            <p>{song.date}</p>
+            <p>{song.album_title}</p>
+            <img src={song.cover_url}/>
+            <p>{song.artist_name}</p>
+            <p>{song.album_title}</p>
+            <p>{song.song_title}</p>
         </div>
     );
 }
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import {initializeApp} from 'firebase/app';
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7bBYMcFoFQRDLmdc5MnDS_RrnsN6xGPo",
-    authDomain: "fun-food-friends-7d9d3.firebaseapp.com",
-    projectId: "fun-food-friends-7d9d3",
-    storageBucket: "fun-food-friends-7d9d3.appspot.com",
-    messagingSenderId: "29947101203",
-    appId: "1:29947101203:web:a67e03beb807395f253fe6"
+    apiKey: "AIzaSyCRPSk2Y5p-Yl6FHrf_3CBc-G30wb3x698",
+    authDomain: "hackathon-fd9ef.firebaseapp.com",
+    projectId: "hackathon-fd9ef",
+    storageBucket: "hackathon-fd9ef.appspot.com",
+    messagingSenderId: "792012328661",
+    appId: "1:792012328661:web:e0f5f17401bbf404ecdc5c"
 };
 
 const app = initializeApp(firebaseConfig);
