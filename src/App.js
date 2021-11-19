16fe030bce3f3dcec43ddc12cb2fa1e1a0457a9a
agakesik <agakesik@gmail.com>
2021-11-19 16:47:24 +0100

    Add changing view based on button clicked

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
+import { useState } from "react";
 import "./App.css";
 import Calendar from "./Calendar/Calendar";
 import Form from "./Form/Form";
 import SongView from "./SongView/SongView";
 
 function App() {
+  const [isFormActive, setIsFormActive] = useState(true);
+  const [isCalActive, setIsCalActive] = useState(false);
+
+  const selectForm = () => {
+    setIsFormActive(true);
+    setIsCalActive(false);
+  };
+
+  const selectCal = () => {
+    setIsFormActive(false);
+    setIsCalActive(true);
+  };
+
   return (
     <div className="layout">
       <h1 className="layout__title">bardzo chwytliwa nazwa</h1>
       <section className="layout__view">
-        <Calendar />
-        <Form />
-        <SongView />
+        {isFormActive ? <Form /> : null}
+        {isCalActive ? <Calendar /> : null}
+        {/* <SongView /> */}
       </section>
 
       <section className="layout__select">
-        <button className="btn btn-outline-dark layout__select__button layout__select__active">
+        <button
+          className={`btn btn-outline-dark layout__select__button ${
+            isFormActive ? "layout__select__active" : ""
+          }`}
+          onClick={selectForm}
+        >
           add
         </button>
-        <button className="btn btn-outline-dark layout__select__button">
+        <button
+          className={`btn btn-outline-dark layout__select__button ${
+            isCalActive ? "layout__select__active" : ""
+          }`}
+          onClick={selectCal}
+        >
           view
         </button>
       </section>
