070c33d521cb3a052016002e717c38938bf6fb05
agakesik <aga.k396@gmail.com>
2021-11-19 21:34:34 +0100

    Add chosing song in form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -7,11 +7,17 @@ import { useEffect } from "react";
 export default function Form() {
   // const { value: artist, bind: bindArtist, reset: resetArtist } = useInput("");
   const [artist, setArtist] = useState({ id: 0, name: "", slug: "" });
-  const { value: song, bind: bindSong, reset: resetSong } = useInput("");
+  const [release, setRelease] = useState({ slug: "jarmark-15698465" });
+  const [song, setSong] = useState("");
+
+  // const { value: song, bind: bindSong, reset: resetSong } = useInput("");
   const { value: day, bind: bindDay, reset: resetDay } = useInput("");
   const [artistList, setArtistList] = useState([]);
+  const [releaseList, setReleaseList] = useState([]);
+  const [songsList, setSongsList] = useState([]);
 
   const ARTISTS_URL = "http://newonce-api.herokuapp.com/artists";
+  const SONGS_URL = "http://newonce-api.herokuapp.com/releases/" + release.slug;
 
   useEffect(() => {
     fetch(ARTISTS_URL, {
@@ -32,8 +38,8 @@ export default function Form() {
   const handleSubmit = (event) => {
     event.preventDefault();
     // resetArtist();
-    resetSong();
-    resetDay();
+    // resetSong();
+    // resetDay();
   };
 
   const chooseArtist = (chosenArtistId) => {
@@ -44,6 +50,25 @@ export default function Form() {
     console.log(chosenArtistId);
     console.log(chosenArtist);
     // fetch artist songs
+
+    fetch(SONGS_URL, {
+      method: "GET",
+    })
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((json) => {
+            console.log(json.tracklist);
+            setSongsList(json.tracklist);
+          });
+        } else {
+          console.log("something went wrong");
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const chooseSong = (chosenSong) => {
+    setSong(chosenSong);
   };
 
   return (
@@ -66,7 +91,16 @@ export default function Form() {
         </div>
         <label>Utwór:</label>
         <div>
-          <input type="text" {...bindSong} />
+          <select onChange={(e) => chooseSong(e.target.value)}>
+            <option value="0"></option>
+            {songsList.length > 0
+              ? songsList.map((song, key) => (
+                  <option value={song.title} key={key}>
+                    {song.title}
+                  </option>
+                ))
+              : null}
+          </select>
         </div>
         <label>Dzień:</label>
         <div>
@@ -76,6 +110,7 @@ export default function Form() {
       </form>
       <div className="test">
         <p>chosen artist: {artist.name}</p>
+        <p>chosen song: {song}</p>
       </div>
     </div>
   );
