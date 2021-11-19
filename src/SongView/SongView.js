33e6bf7cce2fd5c0b8e4f92c233a396c922586c3
agakesik <aga.k396@gmail.com>
2021-11-19 19:35:34 +0100

    clean build

--- src/SongView/SongView.js
+++ src/SongView/SongView.js
@@ -1,25 +1,27 @@
 import React from "react";
 import "regenerator-runtime/runtime.js";
-import {Badge} from "react-bootstrap";
-import {Form} from "react-bootstrap";
+import { Badge } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
-function SongView({song}) {
-
-    return (
-        <div className="songview">
-            <img src={song.cover_url} className="img-thumbnail"/>
-            <h6>
-                <Badge bg="dark"> Artysta:</Badge> <Form.Label>{song.artist_name}</Form.Label>
-            </h6>
-            <h6>
-                <Badge bg="dark">Tytuł albumu:</Badge> <Form.Label>{song.album_title}</Form.Label>
-            </h6>
-            <h6>
-                <Badge bg="dark">Tytuł utworu:</Badge> <Form.Label>{song.song_title}</Form.Label>
-            </h6>
-            <Form.Label>{song.note}</Form.Label>
-        </div>
-    );
+function SongView({ song }) {
+  return (
+    <div className="songview">
+      <img src={song.cover_url} alt="realese cover" className="img-thumbnail" />
+      <h6>
+        <Badge bg="dark"> Artysta:</Badge>{" "}
+        <Form.Label>{song.artist_name}</Form.Label>
+      </h6>
+      <h6>
+        <Badge bg="dark">Tytuł albumu:</Badge>{" "}
+        <Form.Label>{song.album_title}</Form.Label>
+      </h6>
+      <h6>
+        <Badge bg="dark">Tytuł utworu:</Badge>{" "}
+        <Form.Label>{song.song_title}</Form.Label>
+      </h6>
+      <Form.Label>{song.note}</Form.Label>
+    </div>
+  );
 }
 
 export default SongView;
